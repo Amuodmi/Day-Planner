9a512b777020125d5fd3e47de92bffab44166d4a
Michelle <michelle.douma@gmail.com>
2022-06-17 15:24:27 -0700

    fixed color issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ $( function() {
 
 // create array to hold tasks for saving
 var tasksArr = {};
+var tasks = document.getElementsByClassName("task");
 
 //sets time at top of page//
 var displayCurrentTime = document.querySelector("#currentDay");
@@ -17,8 +18,8 @@ displayCurrentTime.textContent = currentTime.format("dddd MMMM Do YYYY");
 
 
 //sets the colours for the time slots whether due, overdue, or pastdue//
-var colourTimeSlots = function (event) {
-  event.preventDefault();
+var colourTimeSlots = function () {
+ 
     var timeSlots = document.getElementsByClassName("task");
     var timeNow = moment().hour();
 
@@ -44,12 +45,10 @@ colourTimeSlots();
 
 
 //saves data to local storage as a string
-var saveTasks = JSON.stringify(task);
-  localStorage.setItem("task", saveTasks);
 
-  // var saveTasks = function() {
-  //   localStorage.setItem("tasks", JSON.stringify(tasks));
-  // };
+  var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
 
 
 
@@ -58,33 +57,33 @@ var loadTasks = function() {
   var savedTasks = JSON.parse(localStorage.getItem("taskArr"));
   console.log(loadTasks);
 
-  if (!savedTasks){
-    return false;
-  }
-  console.log("Saved tasks found!")
+  // if (!savedTasks){
+  //   return false;
+  // }
+  // console.log("Saved tasks found!")
   //else, load up saved tasks
 
   //parse into an array of objects
   savedTasks = JSON.parse(savedTasks);
 
-// if nothing in localStorage, create a new object to track all task status arrays
-if (!tasksArr) {
-  tasksArr = {
-     0900: [],
-     1000: [],
-     1100: [],
-     1200: [],
-     1300: [],
-     1400: [],
-     1500: [],
-     1600: [],
-     1700: [],
-     1800: [],
-     1900: [],
-     2000: [],
-     2100: [],
-  };
-}
+// // if nothing in localStorage, create a new object to track all task status arrays
+// if (!tasksArr) {
+//   tasksArr = {
+//      0900: [],
+//      1000: [],
+//      1100: [],
+//      1200: [],
+//      1300: [],
+//      1400: [],
+//      1500: [],
+//      1600: [],
+//      1700: [],
+//      1800: [],
+//      1900: [],
+//      2000: [],
+//      2100: [],
+//   };
+// }
 };
 
 
